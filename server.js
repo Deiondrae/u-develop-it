7bef43a07273a31388551d6111f318914217a652
Villager <deiondrae@gmail.com>
2022-05-25 15:48:01 -0400

    Added routes on server.js to CRUD database

--- server.js
+++ server.js
@@ -23,8 +23,37 @@ const db = mysql.createConnection(
     console.log("Connected to the election database")
 );
 //runs SQL query and executes callback with rows that match query
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows)
+// Get full list of candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows)
+// });
+
+//Get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 100`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?, ?, ?, ?)`;
+const params = [2, "Virginia", "Woolf", 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 //Route to handle unsupported user requests
